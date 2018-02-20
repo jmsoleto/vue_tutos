71b789f7f05e59267bfa3c4ef1d3de7f75d20b92
Jose Manuel Soleto <josemanuel.soletoaranguez@elcorteingles.es>
2018-02-20 09:30:23 +0100

    #food some components

---
+++ periodicTable/src/components/ListElements.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <div v-for="element in APIData" :key=element.name>
+     {{element.name}}
+   </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ListElements',
+  data () {
+    return {
+
+    }
+  },
+  props:['APIData']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ periodicTable/src/pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <list-elements :APIData="getAPIData"></list-elements>
+  </div>
+</template>
+
+<script>
+const APIData = require('@/assets/data');
+import ListElements from '@/components/ListElements';
+console.log(APIData);
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    getAPIData(){ return APIData}
+  },
+  components: {
+    'list-elements': ListElements,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
